ca28a4238a49b260f8a929f12c0c0a4ee5881107
Thomas <ThommyDude@users.noreply.github.com>
2018-07-04 22:15:37 +0200

    Update config.php
    included the _testing_ entry. It disables downloading the json files from steam.

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
     return [
         'steam_api_key' => ' insert YOUR Steam API key here ',
-        'steam_user_id' => ' insert Steam64 (also known as a 64bit Steam ID) here ' //Technically it could be anyones steamid, but I made it to use my own.
+        'steam_user_id' => ' insert Steam64 (also known as a 64bit Steam ID) here ', //Technically it could be anyones steamid, but I made it to use my own.
+        'testing' => false
     ];
